71e16ef923e930c3ca687a4f0aa328c878b9a5b1
Nethma Dhananjani <64515990+Nethma2000@users.noreply.github.com>
2022-03-24 16:00:06 +0530

    Made changes to available interfaces

--- universitylogin.php
+++ universitylogin.php
@@ -9,12 +9,12 @@ $con= new mysqli('localhost','root','123','careernextgen');
 $email=$_POST['email'];
 $password=$_POST['password'];
 
-$s = "select * from students where email='$email' && password='$password'";
+$s = "select * from universities where university_email='$email' && password='$password'";
 $result=mysqli_query($con,$s);
 $num=mysqli_num_rows($result);
 if($num==1){
     
-        header('location: undergraduatehome.html');
+        header('location: universityhome.html');
    
   
 }
