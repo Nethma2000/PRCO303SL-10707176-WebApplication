82b625f6e21848091e1500861d3d2386eee218da
Nethma Dhananjani <64515990+Nethma2000@users.noreply.github.com>
2022-03-25 13:50:57 +0530

    Created advisor registration

---
+++ advisorsignup.php
@@ -0,0 +1,55 @@
+<?php
+
+$name=$_POST['name'];
+$company=$_POST['company'];
+$designation=$_POST['designation'];
+$field=$_POST['field'];
+$mobileno=$_POST['mobileno'];
+$email=$_POST['email'];
+$password=$_POST['password'];
+$medium=$_POST['medium'];
+$linkedin=$_POST['linkedin'];
+$github=$_POST['github'];
+$qualifications=$_POST['qualifications'];
+$work_experience=$_POST['work_experience'];
+
+
+
+ $con= new mysqli('localhost','root','123','careernextgen');
+if($con->connect_error){
+    die("Connection failed");
+}
+
+else{
+
+    $s = "select * from advisors where email='$email'";
+    $result=mysqli_query($con,$s);
+    $num=mysqli_num_rows($result);
+    if($num==1){
+        echo "Already registered";
+
+    }
+    else{
+        $stmt=$con->prepare("insert into advisors(name,company,designation,field,mobileno,email,password,medium,linkedin,github,qualifications,work_experience)
+        values('$name','$company','$designation','$field','$mobileno','$email','$password','$medium','$linkedin','$github','$qualifications','$work_experience')");
+    
+        $stmt->execute();
+         echo "Registration Successful";
+    //    header('location: home.html');
+
+
+        $stmt->close();
+        $con->close();
+    }
+   
+
+}
+
+
+
+
+
+
+
+
+?>
--- signin copy.php
+++ signin copy.php
@@ -4,14 +4,19 @@ session_start();
 
 $con= new mysqli('localhost','root','123','careernextgen');
 
+
+   
 $email=$_POST['email'];
 $password=$_POST['password'];
 
-$s = "select * from admins where email='$email' && password='$password'";
+$s = "select * from students where email='$email' && password='$password'";
 $result=mysqli_query($con,$s);
 $num=mysqli_num_rows($result);
 if($num==1){
-    header('location: adminhome.html');
+    
+        header('location: undergraduatehome.html');
+   
+  
 }
 else{
     echo "Invalid Email or Password";
